20a6c55d0c6a051e209722a9a799c7754a5ea2ce
Brian Harvey <brian.harv3y@gmail.com>
2020-12-16 10:12:39 -0800

    alter header styling and add new properties to functional component

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,23 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, FlatList, TouchableOpacity, Platform } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import Category from '../models/category';
+import Colors from '../constants/Colors';
 
 const CategoriesScreen = props => {
+  const renderGridItem = (itemData) => {
+    return (
+      <TouchableOpacity style={styles.gridItem} 
+        onPress={() => {
+        props.navigation.navigate(
+          {routeName: 'CategoryMeals'})
+      }}>
+        <View>
+          <Text>{itemData.item.title}</Text>
+        </View>
+      </TouchableOpacity>
+    ) 
+  };
   return (
-    <View style={styles.screen}>
-      <Text>The Categories Screen</Text>
-      <Button title="Go to Meals" onPress={() => {
-        props.navigation.navigate({routeName: 'CategoryMeals'})
-      }}
-      />
-    </View>
-  )
+    <FlatList
+      // keyExtractor={(item, index) => item.id}
+      data={CATEGORIES}
+      renderItem={renderGridItem}
+      numColumns={2}
+    />
+  );
 };
+
+CategoriesScreen.navigationOptions = {
+  headerTitle: 'Meal Categories',
+  headerStyle: {
+    backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+  },
+  headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
+} 
 
 const styles = StyleSheet.create({
 screen: {
   flex: 1,
   justifyContent: 'center',
   alignItems: 'center'
+},
+gridItem: {
+  flex: 1,
+  margin: 15,
+  height: 150
 }
 });
 
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const CategoryMealScreen = props => {
   return (
     <View style={styles.screen}>
       <Text>The Category Meal Screen</Text>
+      <Button title="Meal Details"
+        onPress={() => {
+          props.navigation.navigate('MealDetail') // alt syntax
+          }}
+        />
+      <Button title="Got Back" onPress={() => {
+        props.navigation.goBack(); //could also use pop() but only if in stacknavigator
+      }}
+      />  
     </View>
   )
 };
