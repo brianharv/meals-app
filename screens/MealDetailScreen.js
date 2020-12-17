6e5861c723a295e1b89c4f1b826020b27af6fae2
Brian Harvey <brian.harv3y@gmail.com>
2020-12-16 20:38:53 -0800

    add ingredients list to MealDetailScreen

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,24 +1,30 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../components/HeaderButton';
+import DefaultText from '../components/DefaultText';
 
 const MealDetailScreen = props => {
   const mealId = props.navigation.getParam('mealId');
-
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button title="Go Back to Categories"
-        onPress={() => {
-          props.navigation.popToTop();
-        }}
-      />
-    </View>
+    <ScrollView>
+      <Image  source={{uri: selectedMeal.imageUrl}} style={styles.image}/>
+      <View style={styles.details}>
+        <DefaultText>{selectedMeal.duration}m</DefaultText>
+        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      {selectedMeal.ingredients.map((ingredient, index) => (
+      <Text key={index}>{ingredient}</Text>
+      ))}
+      <Text style={styles.title}>Steps</Text>
+      <Text>List of Steps...</Text>
+    </ScrollView>
   )
 };
 
@@ -40,10 +46,19 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 }
 
 const styles = StyleSheet.create({
-screen: {
-  flex: 1,
-  justifyContent: 'center',
-  alignItems: 'center'
+image: {
+  width: '100%',
+  height: 200
+},
+details: {
+  flexDirection: 'row',
+  padding: 15,
+  justifyContent: 'space-around'
+},
+title: {
+  fontFamily: 'open-sans-bold',
+  fontSize: 22,
+  textAlign: 'center'
 }
 });
 
